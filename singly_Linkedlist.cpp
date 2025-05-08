9562b8856a945c747253364bb9a2fe8e47bda5ef
FarabyMalau <farabymuzakki49@gmail.com>
2025-05-08 18:41:32 +0600

    Proses pengisian data pada node

--- singly_Linkedlist.cpp
+++ singly_Linkedlist.cpp
@@ -15,7 +15,28 @@ class LinkedList
 public:
     LinkedList()
     {
-            START = NULL;
+        START = NULL;
     }
 
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+    }
 }
